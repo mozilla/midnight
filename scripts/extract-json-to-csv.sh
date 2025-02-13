b8d0d0cfd6592d2897c50ed4027421e0ca0c119c
Benjamin De Kosnik <bdekosnik@mozilla.com>
2025-02-12 17:07:39 -0800

    remove obsolete scripts

--- scripts/extract-json-to-csv.sh
+++
@@ -1,1 +0,0 @@
-/home/bkoz/src/mozilla-perf-browsertime-tools/scripts/transform-dir-with-1-metric-cosmology-to-csv.sh ./json ../../../../docs/sitelist.20241022
--- scripts/transform-simplified.sh
+++
@@ -1,3 +0,0 @@
-/home/bkoz/src/mozilla-perf-browsertime-tools/scripts/transform-dir-with-1-metric-cosmology-to-json.sh ./chrome ./sitelist.txt
-
-/home/bkoz/src/mozilla-perf-browsertime-tools/scripts/transform-dir-with-1-metric-cosmology-to-json.sh ./firefox ./sitelist.txt
