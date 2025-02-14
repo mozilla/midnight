969e7578e2b6516377a345acf9be8920af0749cd
Benjamin De Kosnik <bdekosnik@mozilla.com>
2025-02-13 18:16:22 -0800

    update to midnight.sfo-2025-02.3x40

--- scripts/generate-aggregate-json.sh
+++ scripts/generate-aggregate-json.sh
@@ -10,6 +10,12 @@ if [ ! -d tmp ]; then
     mkdir $ODIR
 fi
 
+TPMETADATA1="android-15-p8"
+TPMETADATA2="android-15-ptablet"
+TPMETADATA3="android-14-a55"
+TPMETADATA4="linux-18"
+TPMETADATA5="windows-11"
+
 
 get_aggregate() {
     TESTN="$1"
@@ -22,25 +28,18 @@ get_aggregate() {
 }
 
 #get_aggregate "amazon" "android" "2024-11-15"
-#get_aggregate "amazon" "linux" "2024-11-20"
-#get_aggregate "amazon" "win11" "2024-11-20"
-
-#get_aggregate "amazon" "android" "2024-11-11"
-
-#get_aggregate "allrecipes" "android" "2024-11-20"
-#get_aggregate "espn" "android" "2024-11-20"
-#get_aggregate "micros-sup" "android" "2024-11-20"
 
 # 2
 generate_platform_by_sitelist() {
     PLATFORM="$1"
-    SITELIST="$2"
-    ISODATE="$3"
+    ISODATE="$2"
+    SITELIST="$3"
 
-   MDOWNIDX=index-proto.md
+
+   MDOWNIDX=index-1-col-${PLATFORM}.md
    echo "## Results" >> $MDOWNIDX
 
-   JSIDX=index-proto.js
+   JSIDX=index-${PLATFORM}.js
 
    for i in `cat ${SITELIST}`
    do
@@ -59,7 +58,7 @@ generate_platform_by_sitelist() {
 
        $XAGGREGATE "$URLM" "$i" "$PLATFORM" "$ISODATE" "${ARTIFACT_BASE}-side-by-side.mp4" "$FFFJ" "$FFMJ" "$CFJ" "$CMJ"
 
-       # generate markdown index
+       # generate 1-col markdown index
        echo "- [${URLM}](/pages/${ARTIFACT_BASE}-aggregate.svg)" >> $MDOWNIDX
 
        # generate js index
@@ -67,10 +66,7 @@ generate_platform_by_sitelist() {
    done
 }
 
-#TPMETADATA="android-15-p8"
-#TPMETADATA="linux-18"
-TPMETADATA="windows-11"
-generate_platform_by_sitelist "$TPMETADATA" "./sitelist.txt" "2025-02-12"
+#generate_platform_by_sitelist "$TPMETADATA" "2025-02-12" "./sitelist.txt"
 
 # 3
 generate_data_json() {
@@ -94,4 +90,54 @@ generate_data_json() {
     echo "]" >> $OFILE
 }
 
-generate_data_json
+#generate_data_json
+
+
+# 4
+# NB: FPO only, does not check to make sure files exist before linking to them.
+generate_2_col_index() {
+    PLATFORM1="$1"
+    ISODATE1="$2"
+    PLATFORM2="$3"
+    ISODATE2="$4"
+    SITELIST="$5"
+
+
+    MDOWNIDX2=index-2-col-${PLATFORM1}-${PLATFORM2}.md
+    echo "## Results" >> $MDOWNIDX2
+
+    # create table head
+    echo "<table>" >> $MDOWNIDX2
+    echo "<thead>" >> $MDOWNIDX2
+    echo "<tr>" >> $MDOWNIDX2
+    echo "<th>URL</th>" >> $MDOWNIDX2
+    echo "<th>${PLATFORM1}</th>" >> $MDOWNIDX2
+    echo "<th>${PLATFORM2}</th>" >> $MDOWNIDX2
+    echo "</tr>" >> $MDOWNIDX2
+    echo "</thead>" >> $MDOWNIDX2
+
+    echo "<tbody>" >> $MDOWNIDX2
+
+    for i in `cat ${SITELIST}`
+    do
+	URLM=`${XURLMIN} "$i"`
+	TPLATFORM1="${PLATFORM1}-${URLM}"
+	ARTIFACT_BASE1="${ISODATE1}-${TPLATFORM1}";
+	TPLATFORM2="${PLATFORM2}-${URLM}"
+	ARTIFACT_BASE2="${ISODATE2}-${TPLATFORM2}";
+
+	# generate 2-col html table for markdown embedding
+	echo "<tr>" >> $MDOWNIDX2
+	echo "<td>${URLM}</td>" >> $MDOWNIDX2
+	echo "<td><a href="pages/${ARTIFACT_BASE1}-aggregate.svg">Y</a></td>" >> $MDOWNIDX2
+	echo "<td><a href="pages/${ARTIFACT_BASE2}-aggregate.svg">Y</a></td>" >> $MDOWNIDX2
+	echo "</tr>" >> $MDOWNIDX2
+    done
+
+    echo "</tbody>" >> $MDOWNIDX2
+    echo "</table>" >> $MDOWNIDX2
+}
+
+TPMETADATA_A=$TPMETADATA1
+TPMETADATA_B=$TPMETADATA2
+generate_2_col_index "$TPMETADATA_A" "2025-02-09" "$TPMETADATA_B" "2025-02-10" "./sitelist.txt"
